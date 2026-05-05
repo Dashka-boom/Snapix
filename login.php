732a82b3b30dbd27cec5423f25dbd6cc15dfb310
Pashka-Dashka <iyagamilight70@gmail.com>
2026-05-05 17:13:56 +0300

    Apply unified night-sky theme across Snapix pages

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/auth.css">
-    <title>Вход</title>
+    <title>Snapix</title>
 </head>
 <body data-page="login">
     <main class="auth-page">
